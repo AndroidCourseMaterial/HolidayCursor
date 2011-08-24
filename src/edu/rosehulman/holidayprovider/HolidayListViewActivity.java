d970edba7d2f08b5f3a40f494a24a87e2dd3a915
David Fisher <fisherds@rose-hulman.edu>
2011-08-23 23:04:22 -0700

    Adding new src file locations

---
+++ src/edu/rosehulman/holidayprovider/HolidayListViewActivity.java
@@ -0,0 +1,100 @@
+package edu.rosehulman.holidayprovider;
+
+import android.app.ListActivity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SimpleCursorAdapter;
+
+/**
+ * ListActivity that will display the Holidays in the HolidayProvider
+ * 
+ * @author Dave Fisher
+ *
+ */
+public class HolidayListViewActivity extends ListActivity {
+
+	/**
+	 * Filter string for log messages
+	 */
+	public static final String TAG = "Holidays";
+	
+	// Counter for adding fake new holidays
+	private int mCounter = 0;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.holiday_list_view_activity);
+
+		UsNationalHolidays.populateUsNationalHolidays(this);  // Load default holiday data
+		Uri uri = HolidayProviderMetaData.HolidayTableMetaData.CONTENT_URI;
+
+		Cursor holidayCursor = this.managedQuery(uri, 
+				null, //projection, 
+				null, //selection, 
+				null, //selectionArgs, 
+				null); //sortOrder
+		
+		int viewResourceId = R.layout.row_with_3_items;
+		String[] fromColumns = new String[] {
+				HolidayProviderMetaData.HolidayTableMetaData._ID, 
+				HolidayProviderMetaData.HolidayTableMetaData.HOLIDAY, 
+				HolidayProviderMetaData.HolidayTableMetaData.MONTH, 
+				HolidayProviderMetaData.HolidayTableMetaData.DAY_IN_MONTH, 
+				HolidayProviderMetaData.HolidayTableMetaData.SAME_DATE_EVERY_YEAR,
+				HolidayProviderMetaData.HolidayTableMetaData.OCCURS_ON, 
+				HolidayProviderMetaData.HolidayTableMetaData.APPROX_ORDINAL_DATE
+				};
+		int[] toTextViews = new int[] { 
+				R.id.textView1, 
+				R.id.textView2, 
+				R.id.textView3, 
+				R.id.textView4, 
+				R.id.textView5, 
+				R.id.textView6, 
+				R.id.textView7
+				};
+		SimpleCursorAdapter holidayAdapter = new SimpleCursorAdapter(this, viewResourceId, 
+				holidayCursor, fromColumns, toTextViews);
+		setListAdapter(holidayAdapter);  // Magic function for a ListActivity to set the adapter
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.holiday_list_view_activity_options_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		super.onOptionsItemSelected(item);
+		mCounter++;
+		switch (item.getItemId()) {
+		case R.id.menu_add:
+			Uri uri = HolidayProviderMetaData.HolidayTableMetaData.CONTENT_URI;
+			ContentResolver cr = this.getContentResolver();
+			ContentValues cv = new ContentValues();
+			cv.put(HolidayProviderMetaData.HolidayTableMetaData.HOLIDAY, "Holiday " + mCounter);
+			cv.put(HolidayProviderMetaData.HolidayTableMetaData.MONTH, "January");
+			cv.put(HolidayProviderMetaData.HolidayTableMetaData.DAY_IN_MONTH, mCounter);
+			cv.put(HolidayProviderMetaData.HolidayTableMetaData.SAME_DATE_EVERY_YEAR, 1);
+			cv.put(HolidayProviderMetaData.HolidayTableMetaData.OCCURS_ON, "January " + mCounter);
+			cv.put(HolidayProviderMetaData.HolidayTableMetaData.APPROX_ORDINAL_DATE, mCounter);
+			cr.insert(uri, cv);
+			return true;
+		case R.id.menu_restore_defaults:
+			UsNationalHolidays.resetDefaultHolidayTable();
+			return true;
+		}
+		return false;
+	}
+}
---
+++ src/edu/rosehulman/holidayprovider/HolidayProvider.java
@@ -0,0 +1,258 @@
+package edu.rosehulman.holidayprovider;
+
+import java.util.HashMap;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.text.TextUtils;
+
+/**
+ * The Content Provider that stores holidays
+ * Column names and uri constants available in HolidayProviderMetaData
+ * 
+ * @author Dave Fisher
+ *
+ */
+public class HolidayProvider extends ContentProvider 
+{
+	/**
+	 * Debug label used during development
+	 */
+	public static final String TAG = "HolidayProvider";
+
+	/**
+	 * Default projection that includes all columns
+	 */
+	public static HashMap<String, String> sHolidaysProjectionMap;
+	static 
+	{
+		sHolidaysProjectionMap = new HashMap<String, String>();
+		sHolidaysProjectionMap.put(HolidayProviderMetaData.HolidayTableMetaData._ID, HolidayProviderMetaData.HolidayTableMetaData._ID);
+		sHolidaysProjectionMap.put(HolidayProviderMetaData.HolidayTableMetaData.HOLIDAY, HolidayProviderMetaData.HolidayTableMetaData.HOLIDAY);
+		sHolidaysProjectionMap.put(HolidayProviderMetaData.HolidayTableMetaData.MONTH, HolidayProviderMetaData.HolidayTableMetaData.MONTH);
+		sHolidaysProjectionMap.put(HolidayProviderMetaData.HolidayTableMetaData.DAY_IN_MONTH, HolidayProviderMetaData.HolidayTableMetaData.DAY_IN_MONTH);
+		sHolidaysProjectionMap.put(HolidayProviderMetaData.HolidayTableMetaData.SAME_DATE_EVERY_YEAR, HolidayProviderMetaData.HolidayTableMetaData.SAME_DATE_EVERY_YEAR);
+		sHolidaysProjectionMap.put(HolidayProviderMetaData.HolidayTableMetaData.OCCURS_ON, HolidayProviderMetaData.HolidayTableMetaData.OCCURS_ON);
+		sHolidaysProjectionMap.put(HolidayProviderMetaData.HolidayTableMetaData.APPROX_ORDINAL_DATE, HolidayProviderMetaData.HolidayTableMetaData.APPROX_ORDINAL_DATE);
+	}
+
+	// Used to determine the type of Uri that is being requested
+	private static final UriMatcher sUriMatcher;
+	private static final int INCOMING_URI_INDICATOR_HOLIDAYS = 1;
+	private static final int INCOMING_URI_INDICATOR_SINGLE_HOLIDAY = 2;
+	static {
+		sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+		sUriMatcher.addURI(HolidayProviderMetaData.AUTHORITY, HolidayProviderMetaData.HolidayTableMetaData.TABLE_NAME, INCOMING_URI_INDICATOR_HOLIDAYS);
+		sUriMatcher.addURI(HolidayProviderMetaData.AUTHORITY, HolidayProviderMetaData.HolidayTableMetaData.TABLE_NAME+"/#", INCOMING_URI_INDICATOR_SINGLE_HOLIDAY);
+	}
+
+	// Helper class to open and close the SQLite database
+	private static class HolidayDbOpenHelper extends SQLiteOpenHelper {
+
+		private static String DROP_STATEMENT = "DROP TABLE IF EXISTS " + HolidayProviderMetaData.HolidayTableMetaData.TABLE_NAME;
+		private static String CREATE_STATEMENT;
+		static {
+			StringBuilder s = new StringBuilder();
+			s.append("CREATE TABLE ");
+			s.append(HolidayProviderMetaData.HolidayTableMetaData.TABLE_NAME);
+			s.append(" (");
+			s.append(HolidayProviderMetaData.HolidayTableMetaData._ID);
+			s.append(" INTEGER PRIMARY KEY AUTOINCREMENT, ");
+			s.append(HolidayProviderMetaData.HolidayTableMetaData.HOLIDAY);
+			s.append(" TEXT, ");
+			s.append(HolidayProviderMetaData.HolidayTableMetaData.MONTH);
+			s.append(" TEXT, ");
+			s.append(HolidayProviderMetaData.HolidayTableMetaData.DAY_IN_MONTH);
+			s.append(" INTEGER, ");
+			s.append(HolidayProviderMetaData.HolidayTableMetaData.SAME_DATE_EVERY_YEAR);
+			s.append(" INTEGER, ");
+			s.append(HolidayProviderMetaData.HolidayTableMetaData.OCCURS_ON);
+			s.append(" TEXT, ");
+			s.append(HolidayProviderMetaData.HolidayTableMetaData.APPROX_ORDINAL_DATE);
+			s.append(" INTEGER)");
+			CREATE_STATEMENT = s.toString();
+		}
+
+		public HolidayDbOpenHelper(Context context) {
+			super(context, HolidayProviderMetaData.DATABASE_NAME, null, HolidayProviderMetaData.DATABASE_VERSION);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL(CREATE_STATEMENT);
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			db.execSQL(DROP_STATEMENT);
+			onCreate(db);
+		}
+	}
+
+	private HolidayDbOpenHelper mOpenHelper;
+
+	@Override
+	public boolean onCreate() {
+		mOpenHelper = new HolidayDbOpenHelper(getContext());
+		return true;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+
+		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+		qb.setTables(HolidayProviderMetaData.HolidayTableMetaData.TABLE_NAME);
+		qb.setProjectionMap(sHolidaysProjectionMap);
+
+		switch (sUriMatcher.match(uri)) {
+		case INCOMING_URI_INDICATOR_HOLIDAYS:
+			break;
+		case INCOMING_URI_INDICATOR_SINGLE_HOLIDAY:
+			qb.appendWhere(HolidayProviderMetaData.HolidayTableMetaData._ID + "=" 
+					+ uri.getPathSegments().get(1));
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+
+		// If no sort order is specified use the default
+		String orderBy;
+		if (TextUtils.isEmpty(sortOrder)) {
+			orderBy = HolidayProviderMetaData.HolidayTableMetaData.DEFAULT_SORT_ORDER;
+		} else {
+			orderBy = sortOrder;
+		}
+
+		// Get the database and run the query
+		SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+		Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, orderBy);
+
+		// Tell the cursor what uri to watch, so it knows when its source data changes
+		c.setNotificationUri(getContext().getContentResolver(), uri);
+		return c;
+	}
+
+	/**
+	 * Return the mime type based on the uri request
+	 */
+	@Override
+	public String getType(Uri uri) {
+		switch (sUriMatcher.match(uri)) {
+		case INCOMING_URI_INDICATOR_HOLIDAYS:
+			return HolidayProviderMetaData.HolidayTableMetaData.CONTENT_TYPE;
+		case INCOMING_URI_INDICATOR_SINGLE_HOLIDAY:
+			return HolidayProviderMetaData.HolidayTableMetaData.CONTENT_ITEM_TYPE;
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+	}
+
+	private static final String DEFAULT_MONTH = "January";
+	private static final String DEFAULT_DAY_IN_MONTH = "0";
+
+	/**
+	 * Insert a new holiday and fill in optional columns that are not defined with defaults
+	 */
+	@Override
+	public Uri insert(Uri uri, ContentValues initialValues) {
+		// Validate the requested uri (cannot be the INCOMING_URI_INDICATOR_SINGLE_HOLIDAY)
+		if (sUriMatcher.match(uri) != INCOMING_URI_INDICATOR_HOLIDAYS) {
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+
+		ContentValues values;
+		if (initialValues != null) {
+			values = new ContentValues(initialValues);
+		} else {
+			values = new ContentValues();  // Consider: Just throw an error now
+		}
+
+		// Make sure that the fields are all set.
+		// Optional fields will get filled with a default value
+		// If required fields are missing throw an SQLException
+		if (values.containsKey(HolidayProviderMetaData.HolidayTableMetaData.HOLIDAY) == false) {
+			throw new SQLException("Failed to insert row because HOLIDAY is needed " + uri);
+		}
+		if (values.containsKey(HolidayProviderMetaData.HolidayTableMetaData.MONTH) == false) {
+			values.put(HolidayProviderMetaData.HolidayTableMetaData.MONTH, DEFAULT_MONTH);
+		}
+		if (values.containsKey(HolidayProviderMetaData.HolidayTableMetaData.DAY_IN_MONTH) == false) {
+			values.put(HolidayProviderMetaData.HolidayTableMetaData.DAY_IN_MONTH, DEFAULT_DAY_IN_MONTH);
+		}
+		if (values.containsKey(HolidayProviderMetaData.HolidayTableMetaData.SAME_DATE_EVERY_YEAR) == false) {
+			throw new SQLException("Failed to insert row because SAME_DAY_EVERY_YEAR is needed " + uri);
+		}
+		if (values.containsKey(HolidayProviderMetaData.HolidayTableMetaData.OCCURS_ON) == false) {
+			throw new SQLException("Failed to insert row because OCCURS_ON is needed " + uri);
+		}
+		if (values.containsKey(HolidayProviderMetaData.HolidayTableMetaData.APPROX_ORDINAL_DATE) == false) {
+			throw new SQLException("Failed to insert row because APPROX_ORDINAL_DATE is needed " + uri);
+		}
+
+		// Add the ContentValues as a new row
+		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+		long rowId = db.insert(HolidayProviderMetaData.HolidayTableMetaData.TABLE_NAME, null, values);
+		if (rowId > 0) {
+			Uri insertedHolidayUri = ContentUris.withAppendedId(HolidayProviderMetaData.HolidayTableMetaData.CONTENT_URI, rowId);
+			getContext().getContentResolver().notifyChange(insertedHolidayUri, null);
+			return insertedHolidayUri;
+		}
+		throw new SQLException("Failed to insert row into " + uri);
+	}
+
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+		int count;
+		switch (sUriMatcher.match(uri)) {
+		case INCOMING_URI_INDICATOR_HOLIDAYS:
+			count = db.delete(HolidayProviderMetaData.HolidayTableMetaData.TABLE_NAME, selection, selectionArgs);
+			break;
+		case INCOMING_URI_INDICATOR_SINGLE_HOLIDAY:
+			String rowId = uri.getPathSegments().get(1);
+			count = db.delete(HolidayProviderMetaData.HolidayTableMetaData.TABLE_NAME, 
+					HolidayProviderMetaData.HolidayTableMetaData._ID + "=" + rowId
+					+ (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : ""), 
+					selectionArgs);
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+
+		getContext().getContentResolver().notifyChange(uri, null);
+		return count;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+		int count;
+		switch (sUriMatcher.match(uri)) {
+		case INCOMING_URI_INDICATOR_HOLIDAYS:
+			count = db.update(HolidayProviderMetaData.HolidayTableMetaData.TABLE_NAME, 
+					values, selection, selectionArgs);
+			break;
+		case INCOMING_URI_INDICATOR_SINGLE_HOLIDAY:
+			String rowId = uri.getPathSegments().get(1);
+			count = db.update(HolidayProviderMetaData.HolidayTableMetaData.TABLE_NAME, 
+					values, HolidayProviderMetaData.HolidayTableMetaData._ID + "=" + rowId
+					+ (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : ""), 
+					selectionArgs);
+			break;
+		default:
+			throw new IllegalArgumentException("Unknown URI " + uri);
+		}
+		getContext().getContentResolver().notifyChange(uri, null);
+		return count;
+	}
+}
